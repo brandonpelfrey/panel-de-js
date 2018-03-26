a16a782f476e4c9f4325286b5a35af69ea6d41ea
Brandon Pelfrey <mark.zingler@gmail.com>
2018-03-25 17:31:37 -0700

    Adding TrashBlock class

---
+++ src/trashBlock.js
@@ -0,0 +1,18 @@
+class TrashBlock {
+
+	// x and y represent the top left hand corner of the trash block
+  constructor({ x, y, width = 3, height = 1 } = {}) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  *positions() {
+  	for(let i=0; i<width; i++) {
+  		for(let j=0; j<height; j++) {
+  			yield [x+i, y+j];
+  		}
+  	}
+  }
+}
